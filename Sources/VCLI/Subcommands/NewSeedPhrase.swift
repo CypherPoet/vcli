8c616e784609782893884fabef8111c73e702260
CypherPoet <CypherPoet@tutanota.com>
2022-07-29 11:40:37 -0500

    Change length of new-seed-phrase to word-count.
    Closes https://github.com/valeralabs/vcli/issues/1

--- Sources/VCLI/Subcommands/NewSeedPhrase.swift
+++ Sources/VCLI/Subcommands/NewSeedPhrase.swift
@@ -4,26 +4,48 @@ import VDK
 
 
 extension VCLI {
-
+    
     public struct NewSeedPhrase: ParsableCommand {
+
+        // MARK: - Init
         
         public init() {}
-        
+
+
+        // MARK: - ParsableCommand
+
         public static var configuration = CommandConfiguration(abstract: "create a new seed phrase.")
         
-        @Option(name: [.customLong("length"), .customShort("l")], help: "total words in seed phrase, between 12 - 24.")
+        
+        @Option(
+            name: [
+                .customLong("word-count"),
+            ],
+            help: "The total number of words in seed phrase, between 12 - 24."
+        )
         public var length = 24
+        
         
         public func run() throws {
             var error: NSError?
             
             let phrase = VDKNewPhrase(length, &error)
             
-            if error != nil {
-                throw error!
+            if let error {
+                throw Error.newPhraseCreationFailed(error: error)
             }
-            
+
             print(phrase)
         }
+    }
+}
+
+
+// MARK: - Error Handling
+
+extension VCLI.NewSeedPhrase {
+    
+    enum Error: Swift.Error {
+        case newPhraseCreationFailed(error: Swift.Error)
     }
 }
